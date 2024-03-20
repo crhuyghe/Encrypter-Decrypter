48a90eb747ff6047d499ad9838302441855b13c3
PlutoPenguin11 <124006402+PlutoPenguin11@users.noreply.github.com>
2024-03-20 00:46:17 -0400

    Update elliptic_curve.py
    Simplified elliptic curve code

--- encryption/elliptic_curve.py
+++ encryption/elliptic_curve.py
@@ -1,53 +1,35 @@
 # Curve characteristics as detailed by SECG's secp128r2
 
-class EllipticCurve:
-    def __init__(self, p, a, b, g_x, g_y, order, cofactor):
-        self.p = p
-        self.a = a
-        self.b = b
-        self.g_x = g_x
-        self.g_y = g_y
-        self.order = order
-        self.cofactor = cofactor
+p = 0xfffffffdffffffffffffffffffffffff
+a = 0xd6031998d1b3bbfebf59cc9bbff9aee1
+G = (0x7b6aa5d85e572983e6fb32a7cdebc140, 0x27b6916a894d3aee7106fe805fc34b44)
 
-    def point_add(self, P, Q):
+def scalarMultiply(k, P):
+    def pointAdd(P, Q):
         if P is None:
             return Q
         if Q is None:
             return P
 
         if P == Q:
-            lam = (3 * P[0] * P[0] + self.a) * pow(2 * P[1], -1, self.p)
+            lam = (3 * P[0] * P[0] + a) * pow(2 * P[1], -1, p)
         else:
-            lam = (Q[1] - P[1]) * pow(Q[0] - P[0], -1, self.p)
+            lam = (Q[1] - P[1]) * pow(Q[0] - P[0], -1, p)
 
-        x = (lam * lam - P[0] - Q[0]) % self.p
-        y = (lam * (P[0] - x) - P[1]) % self.p
+        x = (lam * lam - P[0] - Q[0]) % p
+        y = (lam * (P[0] - x) - P[1]) % p
 
         return (x, y)
 
-    def scalar_multiply(self, k, P):
-        result = None
-        for bit in bin(k)[2:]:
-            result = self.point_add(result, result)
-            if bit == '1':
-                result = self.point_add(result, P)
-        return result
-
-curve_params = {
-    "p": 0xfffffffdffffffffffffffffffffffff,
-    "a": 0xd6031998d1b3bbfebf59cc9bbff9aee1,
-    "b": 0x5eeefca380d02919dc2c6558bb6d8a5d,
-    "g_x": 0x7b6aa5d85e572983e6fb32a7cdebc140,
-    "g_y": 0x27b6916a894d3aee7106fe805fc34b44,
-    "order": 0x3fffffff7fffffffbe0024720613b5a3,
-    "cofactor": 0x04,
-}
-
-curve = EllipticCurve(**curve_params)
+    result = None
+    for bit in bin(k)[2:]:
+        result = pointAdd(result, result)
+        if bit == '1':
+            result = pointAdd(result, P)
+    return result
 
 def generatePublicKey(private):
-    return curve.scalar_multiply(private, (curve_params["g_x"], curve_params["g_y"]))
+    return scalarMultiply(private, G)
 
 def generateSharedSecret(private, coordinates):
-    return curve.scalar_multiply(private, coordinates)
+    return scalarMultiply(private, coordinates)
